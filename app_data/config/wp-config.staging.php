f68d1271d207023842bd01bc48a132dc7cf3a6a3
Alex Lana <alexlana@gmail.com>
2022-09-15 01:52:37 -0300

    ...

--- app_data/config/wp-config.staging.php
+++ app_data/config/wp-config.staging.php
@@ -152,7 +152,7 @@ $table_prefix = 'alxwp_';
 
 define( 'WP_DEBUG', true );
 error_reporting(E_ALL);
-ini_set('display_errors', true);
+ini_set('display_errors', true); 
 
 
 /* Add any custom values between this line and the "stop editing" line. */
