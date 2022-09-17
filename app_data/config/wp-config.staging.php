233b46f1aba53e5f0cadfa271f0ec717f056e711
Alex Lana <alexlana@gmail.com>
2022-09-17 01:12:46 -0300

    ...

--- app_data/config/wp-config.staging.php
+++ app_data/config/wp-config.staging.php
@@ -38,34 +38,31 @@
 
 
 // ** Database settings - You can get this info from your web host ** //
+// /** The name of the database for WordPress */
+// define( 'DB_NAME', getenv('WORDPRESS_DATABASE_NAME') );
+// /** Database username */
+// define( 'DB_USER', getenv('WORDPRESS_DATABASE_USER') );
+// /** Database password */
+// define( 'DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD') );
+// /** Database hostname */
+// define( 'DB_HOST', getenv('WORDPRESS_DATABASE_HOST') );
+// /** Database charset to use in creating database tables. */
+// define( 'DB_CHARSET', 'utf8' );
 
 /** The name of the database for WordPress */
-
-define( 'DB_NAME', getenv('WORDPRESS_DATABASE_NAME') );
-
-
+define( 'DB_NAME', 'alx-wp-gcp' );
 /** Database username */
-
-define( 'DB_USER', getenv('WORDPRESS_DATABASE_USER') );
-
-
+define( 'DB_USER', 'alx-wp-gcp' );
 /** Database password */
-
-define( 'DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD') );
-
-
+define( 'DB_PASSWORD', 'e0Y8=Z1TAX\l@nAu' );
 /** Database hostname */
-
-define( 'DB_HOST', getenv('WORDPRESS_DATABASE_HOST') );
-
-
+define( 'DB_HOST', '34.172.252.255' );
 /** Database charset to use in creating database tables. */
-
 define( 'DB_CHARSET', 'utf8' );
 
 
-/** The database collate type. Don't change this if in doubt. */
 
+/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
 
